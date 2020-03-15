cd81634cdd404d5707f6d47522b24aea1c901494
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-03-15 18:28:10 -0400

    add fixture for six sided die

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import os
 
 import pytest
+import thinkbayes
+from thinkbayes import Hist, Pmf, Suite
 from thinkbayes.utils import read_gss
 
 TESTDIR = os.path.abspath(os.path.dirname(__file__))
@@ -13,3 +15,17 @@ def gss():
     https://gssdataexplorer.norc.org/projects/52787/variables/1698/vshow
     """
     return read_gss(os.path.join(DATADIR, "gss_bayes"))
+
+
+@pytest.fixture
+def six_sided_die_pmf():
+    """
+    a probability mass function that represents the outcome of a six-sided die.
+    Initially there are 6 values with equal probability.
+    :return:
+    """
+    pmf = thinkbayes.Pmf()
+    for x in [1, 2, 3, 4, 5, 6]:
+        pmf[x] = 1
+    pmf.Normalize()
+    return pmf
