184f0933861508b5ef7ccd96acf8168bd144a809
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-03-15 18:02:44 -0400

    refactor notebook into a pytest

--- tests/test_code/test_chap01.py
+++ tests/test_code/test_chap01.py
@@ -1,121 +1,13 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.4.0
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
-# # Think Bayes
-#
-# This notebook presents example code and exercise solutions for Think Bayes.
-#
-# Copyright 2018 Allen B. Downey
-#
-# MIT License: https://opensource.org/licenses/MIT
-
-# +
-# Configure Jupyter so figures appear in the notebook
-# %matplotlib inline
-
-# Configure Jupyter to display the assigned value after an assignment
-# %config InteractiveShell.ast_node_interactivity='last_expr_or_assign'
+"""
+This notebook presents example code and exercise solutions for Think Bayes.
+"""
 
 import pandas as pd
 import numpy as np
 
-# +
-from thinkbayes.utils import read_gss
-
-gss = read_gss("../../tests/data/gss_bayes")
-gss.head()
-
-
-# +
-def replace_invalid(series, bad_vals, replacement=np.nan):
-    series.replace(bad_vals, replacement, inplace=True)
-
-
-replace_invalid(gss.feminist, [0, 8, 9])
-replace_invalid(gss.polviews, [0, 8, 9])
-replace_invalid(gss.partyid, [8, 9])
-replace_invalid(gss.indus10, [0])
-replace_invalid(gss.occ10, [0])
-
-
-# -
-
 
 def values(series):
     return series.value_counts().sort_index()
-
-
-# https://gssdataexplorer.norc.org/projects/52787/variables/1698/vshow
-
-values(gss.feminist)
-
-# https://gssdataexplorer.norc.org/projects/52787/variables/178/vshow
-
-values(gss.polviews)
-
-# https://gssdataexplorer.norc.org/projects/52787/variables/141/vshow
-
-values(gss.partyid)
-
-# https://gssdataexplorer.norc.org/projects/52787/variables/82/vshow
-
-values(gss.race)
-
-# https://gssdataexplorer.norc.org/projects/52787/variables/81/vshow
-
-values(gss.sex)
-
-# https://gssdataexplorer.norc.org/projects/52787/variables/17/vshow
-#
-# 6870	Banking and related activities
-
-values(gss.indus10).head()
-
-np.mean(gss.indus10 == 6870)
-
-(gss.indus10 == 6870).mean()
-
-subset = gss.dropna(subset=["sex", "polviews", "partyid", "indus10"])
-subset.shape
-
-#
-
-
-#
-
-female = gss.sex == 2
-values(female)
-
-liberal = gss.polviews <= 2
-values(liberal)
-
-democrat = gss.partyid <= 1
-values(democrat)
-
-banker = gss.indus10 == 6870
-values(banker)
-
-# +
-total = 0
-for x in banker:
-    if x is True:
-        total += 1
-
-total
-# -
-
-total / len(banker)
 
 
 def prob(A):
@@ -128,37 +20,9 @@ def count(A):
     return A.sum()
 
 
-prob(female)
-
-prob(liberal)
-
-prob(democrat)
-
-prob(banker)
-
-prob(democrat & liberal)
-
-count(banker[female])
-
-prob(banker[female])
-
-prob(female & banker)
-
-prob(banker & female) / prob(female)
-
-
 def conditional(A, B):
     """Conditional probability of A given B"""
     return prob(A[B])
-
-
-conditional(banker, female)
-
-conditional(liberal, democrat)
-
-conditional(democrat, liberal)
-
-conditional(democrat, female)
 
 
 def conjunction(A, B):
@@ -166,26 +30,90 @@ def conjunction(A, B):
     return prob(A) * conditional(B, A)
 
 
-prob(liberal & democrat)
-
-conjunction(liberal, democrat)
-
-prob(liberal) * prob(democrat)
-
-conjunction(democrat, liberal)
-
-prob(banker) * conditional(female, banker) / prob(female)
-
-
 def bayes_theorem(A, B):
     """Conditional probability of A given B, using Bayes's theorem"""
     return prob(A) * conditional(B, A) / prob(B)
 
 
-bayes_theorem(democrat, liberal)
+def test_total(gss):
+    gss.feminist.replace([0, 8, 9], np.nan, inplace=True)
+    gss.polviews.replace([0, 8, 9], np.nan, inplace=True)
+    gss.partyid.replace([8, 9], np.nan, inplace=True)
+    gss.indus10.replace([0], np.nan, inplace=True)
+    gss.occ10.replace([0], np.nan, inplace=True)
 
-conditional(banker, female)
+    values(gss.feminist)
+    values(gss.polviews)
+    values(gss.partyid)
+    values(gss.race)
+    values(gss.sex)
+    values(gss.indus10).head()
+    np.mean(gss.indus10 == 6870)
 
-conditional(banker, female & liberal)
+    (gss.indus10 == 6870).mean()
 
-conditional(banker & democrat, female & liberal)
+    subset = gss.dropna(subset=["sex", "polviews", "partyid", "indus10"])
+    subset.shape
+
+    female = gss.sex == 2
+    values(female)
+
+    liberal = gss.polviews <= 2
+    values(liberal)
+
+    democrat = gss.partyid <= 1
+    values(democrat)
+
+    banker = gss.indus10 == 6870
+    values(banker)
+
+    total = 0
+    for x in banker:
+        if x is True:
+            total += 1
+
+    total / len(banker)
+
+    prob(female)
+
+    prob(liberal)
+
+    prob(democrat)
+
+    prob(banker)
+
+    prob(democrat & liberal)
+
+    count(banker[female])
+
+    prob(banker[female])
+
+    prob(female & banker)
+
+    prob(banker & female) / prob(female)
+
+    conditional(banker, female)
+
+    conditional(liberal, democrat)
+
+    conditional(democrat, liberal)
+
+    conditional(democrat, female)
+
+    prob(liberal & democrat)
+
+    conjunction(liberal, democrat)
+
+    prob(liberal) * prob(democrat)
+
+    conjunction(democrat, liberal)
+
+    prob(banker) * conditional(female, banker) / prob(female)
+
+    bayes_theorem(democrat, liberal)
+
+    conditional(banker, female)
+
+    conditional(banker, female & liberal)
+
+    conditional(banker & democrat, female & liberal)
