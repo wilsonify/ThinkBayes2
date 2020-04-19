8af935cfd6d9d546cb8179bed9cca19191a52b8d
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-04-19 16:42:34 -0400

    remove jupytext header

--- tests/test_examples/test_btp01soln.py
+++ tests/test_examples/test_btp01soln.py
@@ -1,14 +1,4 @@
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.4.0
-#   kernelspec:
-#     display_name: Python 2
-#     language: python
-#     name: python2
+
 
 
 # # Bayes Theorem Problems
