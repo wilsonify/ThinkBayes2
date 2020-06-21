d3d14e2e80ba998b26f85eec19ff221502e20199
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-06-21 06:13:20 -0400

    simplify string

--- src/think-bayes/thinkbayes/scripts/cookie.py
+++ src/think-bayes/thinkbayes/scripts/cookie.py
@@ -8,12 +8,12 @@ MIT License: https://opensource.org/licenses/MIT
 from thinkbayes import Pmf
 
 pmf = Pmf()
-pmf.Set("Bowl 1", 0.5)
-pmf.Set("Bowl 2", 0.5)
+pmf.Set("Bowl1", 0.5)
+pmf.Set("Bowl2", 0.5)
 
-pmf.Mult("Bowl 1", 0.75)
-pmf.Mult("Bowl 2", 0.5)
+pmf.Mult("Bowl1", 0.75)
+pmf.Mult("Bowl2", 0.5)
 
 pmf.Normalize()
 
-print(pmf.Prob("Bowl 1"))
+print(pmf.Prob("Bowl1"))
