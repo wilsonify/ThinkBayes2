a5d53d6370af9931de663d8d6d25c38d3afbde22
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-06-27 10:46:50 -0400

    remove d6 fixture

--- tests/test_code/conftest.py
+++ tests/test_code/conftest.py
@@ -3,7 +3,6 @@ import os
 import pandas as pd
 import pytest
 import thinkbayes
-from thinkbayes import Pmf
 from thinkbayes.utils import read_gss
 
 CONFTESTDIR = os.path.abspath(os.path.dirname(__file__))
@@ -43,50 +42,3 @@ def drp_scores_df():
 @pytest.fixture
 def flea_beetles_df():
     return pd.read_csv(os.path.join(DATADIR, "flea_beetles.csv"), delimiter="\t")
-
-
-@pytest.fixture(name="d6")
-def d6_fixture():
-    """
-        Playing dice with the universe
-    One of the recurring themes of my books is the use of object-oriented programming to explore mathematical ideas.
-    Many mathematical entities are hard to define because they are so abstract.
-    Representing them in Python puts the focus on what operations each entity supports --
-    that is, what the objects can *do* -- rather than on what they *are*.
-    In this notebook, I explore the idea of a probability distribution,
-    which is one of the most important ideas in statistics, but also one of the hardest to explain.
-
-    To keep things concrete, I'll start with one of the usual examples: rolling dice.
-    When you roll a standard six-sided die,
-    there are six possible outcomes -- numbers 1 through 6 -- and all outcomes are equally likely.
-
-    If you roll two dice and add up the total, there are 11 possible outcomes
-    -- numbers 2 through 12 -- but they are not equally likely.
-    The least likely outcomes, 2 and 12, only happen once in 36 tries;
-    the most likely outcome happens 1 times in 6.
-
-    And if you roll three dice and add them up,
-    you get a different set of possible outcomes with a different set of probabilities.
-
-    What I've just described are three random number generators,
-    which are also called **random processes**.
-    The output from a random process is a **random variable**,
-    or more generally a set of random variables.
-    And each random variable has **probability distribution**,
-    which is the set of possible outcomes and the corresponding set of probabilities.
-
-    There are many ways to represent a probability distribution.
-    The most obvious is a **probability mass function**, or PMF,
-    which is a function that maps from each possible outcome to its probability.
-    And in Python, the most obvious way to represent a PMF is a dictionary that maps from outcomes to probabilities.
-
-    `thinkbayes2` provides a class called `Pmf` that represents a probability mass function.
-    Each `Pmf` contains a dictionary named `d` that contains the values and probabilities.
-    To show how this class is used, I'll create a `Pmf` that represents a six-sided die:
-    :return:
-    """
-    d6 = Pmf()
-    for x in range(1, 7):
-        d6[x] = 1
-    d6.normalize()
-    return d6
