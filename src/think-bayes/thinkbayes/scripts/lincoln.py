a72fda58a611fd249c92765bbdab267ccd1d19df
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-06-27 13:31:00 -0400

    remove duplicate code

--- src/think-bayes/thinkbayes/scripts/lincoln.py
+++ src/think-bayes/thinkbayes/scripts/lincoln.py
@@ -3,14 +3,7 @@ by Allen B. Downey, available from greenteapress.com
 
 Copyright 2014 Allen B. Downey
 MIT License: https://opensource.org/licenses/MIT
-"""
 
-import numpy
-
-import thinkbayes
-from thinkbayes import thinkplot
-
-"""
 Bayesian solution to the Lincoln index, described in a blog
 article at Probably Overthinking It.
 
@@ -19,13 +12,13 @@ https://tinyurl.com/lincoln14
 Last year my occasional correspondent John D. Cook wrote an excellent
 blog post about the Lincoln index, which is a way to estimate the
 number of errors in a document (or program) by comparing results from
-two independent testers. 
+two independent testers.
 
 https://www.johndcook.com/blog/2010/07/13/lincoln-index/
 
 Here's his presentation of the problem:
 
-   "Suppose you have a tester who finds 20 bugs in your program. You
+"Suppose you have a tester who finds 20 bugs in your program. You
 want to estimate how many bugs are really in the program. You know
 there are at least 20 bugs, and if you have supreme confidence in your
 tester, you may suppose there are around 20 bugs. But maybe your
@@ -39,7 +32,7 @@ Frederick Charles Lincoln in 1930," where Wikpedia's use of
 "described" is a hint that the index is another example of Stigler's
 law of eponymy.
 
-    "Suppose two testers independently search for bugs. Let k1 be the
+"Suppose two testers independently search for bugs. Let k1 be the
 number of errors the first tester finds and k2 the number of errors
 the second tester finds. Let c be the number of errors both testers
 find. The Lincoln Index estimates the total number of errors as k1 k2
@@ -51,8 +44,11 @@ find 3 in common, we estimate that there are about 100 bugs.
 Of course, whenever I see something like this, the idea that pops into
 my head is that there must be a (better) Bayesian solution! And there
 is.
-
 """
+
+import thinkbayes
+import numpy
+from thinkbayes import thinkplot
 
 
 def choose(n, k, d=None):
@@ -108,7 +104,46 @@ class Lincoln(thinkbayes.Suite, thinkbayes.Joint):
         return part1 * part2
 
 
+
+
 def main():
+    data = 20, 15, 3
+    probs = numpy.linspace(0, 1, 31)
+    hypos = []
+    for n in range(32, 350):
+        for p1 in probs:
+            for p2 in probs:
+                hypos.append((n, p1, p2))
+
+    suite = Lincoln(hypos)
+    suite.update(data)
+
+    n_marginal = suite.marginal(0)
+
+    thinkplot.plot_pmf_line(n_marginal, label="n")
+    thinkplot.save_plot(
+        root="lincoln1", xlabel="number of bugs", ylabel="PMF", formats=["pdf", "png"]
+    )
+
+    print("post mean n", n_marginal.mean())
+    print("MAP n", n_marginal.MaximumLikelihood())
+
+    p1_marginal = suite.marginal(1, label="p1")
+    p2_marginal = suite.marginal(2, label="p2")
+
+    thinkplot.plot_pdf_line(p1_marginal)
+    thinkplot.plot_pdf_line(p2_marginal)
+    thinkplot.show_plot()
+
+    print("post mean p1", p1_marginal.mean())
+    print("MAP p1", p1_marginal.MaximumLikelihood())
+
+    print("post mean p2", p2_marginal.mean())
+    print("MAP p2", p2_marginal.MaximumLikelihood())
+
+    print("p1 > p2", p1_marginal > p2_marginal)
+    print("p1 < p2", p1_marginal < p2_marginal)
+
     data = 20, 15, 3
     probs = numpy.linspace(0, 1, 31)
     hypos = []
