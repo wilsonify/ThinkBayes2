230c0263a33659e04847dec2fefded4bef6a1221
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-03-06 18:23:09 -0500

    better import test

--- tests/test_ppbm.py
+++ tests/test_ppbm.py
@@ -9,7 +9,6 @@ from ppbm.Chapter3_MCMC import Ch3_IntroMCMC_PyMC3
 from ppbm.Chapter4_TheGreatestTheoremNeverTold import Ch4_LawOfLargeNumbers_PyMC3
 from ppbm.Chapter5_LossFunctions import Ch5_LossFunctions_PyMC3
 from ppbm.Chapter6_Priorities import Ch6_Priors_PyMC3
-from ppbm.Chapter7_BayesianMachineLearning import MachineLearning
 
 
 def test_smoke():
@@ -22,4 +21,3 @@ def test_smoke():
     print(dir(Ch4_LawOfLargeNumbers_PyMC3))
     print(dir(Ch5_LossFunctions_PyMC3))
     print(dir(Ch6_Priors_PyMC3))
-    print(dir(MachineLearning))
