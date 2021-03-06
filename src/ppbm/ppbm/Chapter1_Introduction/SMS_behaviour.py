2c967466932f2996061bd671ecb66f947b633daf
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-03-06 15:56:28 -0500

    working examples

--- src/ppbm/ppbm/Chapter1_Introduction/SMS_behaviour.py
+++ src/ppbm/ppbm/Chapter1_Introduction/SMS_behaviour.py
@@ -1,11 +1,11 @@
-import pymc as pm
 import numpy as np
+import pymc as pm
 
 count_data = np.loadtxt("../../Chapter1_Introduction/data/txtdata.csv")
 n_count_data = len(count_data)
 
 alpha = 1.0 / count_data.mean()  # recall count_data is
-                                 # the variable that holds our txt counts
+# the variable that holds our txt counts
 
 lambda_1 = pm.Exponential("lambda_1", alpha)
 lambda_2 = pm.Exponential("lambda_2", alpha)
@@ -20,9 +20,9 @@ def lambda_(tau=tau, lambda_1=lambda_1, lambda_2=lambda_2):
     out[tau:] = lambda_2  # lambda after tau is lambda2
     return out
 
+
 observation = pm.Poisson("obs", lambda_, value=count_data, observed=True)
 model = pm.Model([observation, lambda_1, lambda_2, tau])
-
 
 mcmc = pm.MCMC(model)
 mcmc.sample(100000, 50000, 1)
