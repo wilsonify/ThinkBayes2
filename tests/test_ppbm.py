372c9375fb3edf8c716265b6bbe6c19118a7c343
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-03-06 14:03:54 -0500

    better smoke test

--- tests/test_ppbm.py
+++ tests/test_ppbm.py
@@ -10,10 +10,15 @@ from ppbm import (
     Chapter7_BayesianMachineLearning
 )
 
+
 def test_smoke():
     print("is anything on fire?")
     print(dir(ppbm))
-
-
-
-
+    print(dir(Chapter0_Prologue))
+    print(dir(Chapter1_Introduction))
+    print(dir(Chapter2_MorePyMC))
+    print(dir(Chapter3_MCMC))
+    print(dir(Chapter4_TheGreatestTheoremNeverTold))
+    print(dir(Chapter5_LossFunctions))
+    print(dir(Chapter6_Priorities))
+    print(dir(Chapter7_BayesianMachineLearning))
