2c9c5ee13f2bbaa525bfc4b30614d1395c24a178
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-03-06 14:00:04 -0500

    create test

---
+++ tests/test_ppbm.py
@@ -0,0 +1,19 @@
+import ppbm
+from ppbm import (
+    Chapter0_Prologue,
+    Chapter1_Introduction,
+    Chapter2_MorePyMC,
+    Chapter3_MCMC,
+    Chapter4_TheGreatestTheoremNeverTold,
+    Chapter5_LossFunctions,
+    Chapter6_Priorities,
+    Chapter7_BayesianMachineLearning
+)
+
+def test_smoke():
+    print("is anything on fire?")
+    print(dir(ppbm))
+
+
+
+
