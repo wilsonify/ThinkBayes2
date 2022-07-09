e68243b63c62d08014ac4d2c477b7c3db3369405
Tom Wilson <tom.andrew.wilson@gmail.com>
2022-07-09 09:07:40 -0400

    split plotting to separate package split sacred to separate package

---
+++ src/think-plot/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="thinkplot",
+    version="2.0.0",
+    author="Allen B. Downey",
+    author_email="downey@allendowney.com",
+    packages=find_packages(),
+    url="https://github.com/AllenDowney/thinkbayes",
+    license="LICENSE.txt",
+    description="Supporting code for the book Think Bayes 2e.",
+)
