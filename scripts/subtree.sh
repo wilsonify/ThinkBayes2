c3c1dda2261bf620f6facc6f26dea547872c9d73
wilsonify <tom.andrew.wilson@gmail.com>
2025-11-03 07:52:29 -0500

    reorg

--- scripts/subtree.sh
+++
@@ -1,5 +0,0 @@
-# consolidate repos by adding subtree to prefix folder
-git subtree add \
---prefix src/ppbm \
-https://github.com/wilsonify/Probabilistic-Programming-and-Bayesian-Methods-for-Hackers.git develop \
-# --squash
